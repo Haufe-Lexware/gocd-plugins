53ea44e90a3f7695490f4587a7326f602ec119a1
bradeac <bradeacamil@gmail.com>
2017-07-10 10:40:31 +0300

    Sonar plugin: removed unnecessary auth

--- sonar-qualitygates-plugin/src/com/tw/go/task/sonarqualitygate/SonarTaskExecutor.java
+++ sonar-qualitygates-plugin/src/com/tw/go/task/sonarqualitygate/SonarTaskExecutor.java
@@ -33,16 +33,16 @@ public class SonarTaskExecutor extends TaskExecutor {
 
             SonarClient sonarClient = new SonarClient(sonarApiUrl);
 
-            // This might need some auth!
-            Map envVars = context.getEnvironmentVariables();
-            if (envVars.get(GoApiConstants.ENVVAR_NAME_GO_BUILD_USER) != null &&
-                    envVars.get(GoApiConstants.ENVVAR_NAME_GO_BUILD_USER_PASSWORD) != null) {
-
-                sonarClient.setBasicAuthentication(envVars.get(GoApiConstants.ENVVAR_NAME_GO_BUILD_USER).toString(), envVars.get(GoApiConstants.ENVVAR_NAME_GO_BUILD_USER_PASSWORD).toString());
-                log("Logged in as '" + envVars.get(GoApiConstants.ENVVAR_NAME_GO_BUILD_USER).toString() + "' to get the project's quality gate");
-            } else {
-                log(" Requesting project's quality gate anonymously.");
-            }
+//            // This might need some auth!
+//            Map envVars = context.getEnvironmentVariables();
+//            if (envVars.get(GoApiConstants.ENVVAR_NAME_SONAR_USER) != null &&
+//                    envVars.get(GoApiConstants.ENVVAR_NAME_SONAR_USER_PASSWORD) != null) {
+//
+//                sonarClient.setBasicAuthentication(envVars.get(GoApiConstants.ENVVAR_NAME_SONAR_USER).toString(), envVars.get(GoApiConstants.ENVVAR_NAME_SONAR_USER_PASSWORD).toString());
+//                log("Logged in as '" + envVars.get(GoApiConstants.ENVVAR_NAME_SONAR_USER).toString() + "' to get the project's quality gate");
+//            } else {
+//                log(" Requesting project's quality gate anonymously.");
+//            }
 
             // get quality gate details
             JSONObject result = sonarClient.getProjectWithQualityGateDetails(sonarProjectKey);
